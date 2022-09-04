b0e99556f52efcc8f21373f19524daddbc50062b
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-04 17:01:19 -0300

    removing example route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,10 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
 import { itemsRouter } from "./items";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
   .merge("items.", itemsRouter);
 
 // export type definition of API
